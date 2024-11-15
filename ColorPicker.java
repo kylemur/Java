2ea2cfaa31190cac3fd5fb8c0fcc1c3cb9e596dd
Kyle Murdock <166570055+kylemur@users.noreply.github.com>
2024-11-14 17:39:50 -0700

    made empty classes and a GUI with buttons

---
+++ ColorPicker.java
@@ -0,0 +1,5 @@
+public class ColorPicker {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Shape.java
@@ -0,0 +1,15 @@
+public class Shape {
+    
+}
+
+class RectangleShape extends Shape {
+    
+}
+
+class OvalShape extends Shape {
+    
+}
+
+class TriangleShape extends Shape {
+    
+}
